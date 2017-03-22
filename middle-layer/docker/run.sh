4e0c3091de2d93a94dd85e2f2173a03a324bb5ad
Andriy Dumchykov <Andriy_Dumchykov@epam.com>
2017-03-22 20:08:09 +0200

    hadoop claster

--- middle-layer/docker/run.sh
+++ middle-layer/docker/run.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-
-#mvn dependency:get \
-#    -DrepoUrl=http://.../ \
-#        -Dartifact=org.hibernate:hibernate-entitymanager:3.4.0.GA:jar \
-#        -Dtransitive=false \
-#        -Ddest=component.jar \
+mvn clean install
 
 mvn dependency:get \
     -DrepoUrl=http://.../ \
         -Dartifact=com.myproj:spring-boot:0.5-SNAPSHOT:war \
         -Dtransitive=false \
         -Ddest=mvc-app.war
-
 
 docker-compose up
